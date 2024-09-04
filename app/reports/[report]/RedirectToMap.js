389b253a50380405aa60edb4d672e37de3b431dd
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-09-05 03:47:59 +0400

    Update RedirectToMap.js

--- app/reports/[report]/RedirectToMap.js
+++ app/reports/[report]/RedirectToMap.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import React from 'react'
 
 function RedirectToMap({lat,lng}) {
-    const url = `https://google.com/maps?q=${lat},${lng}`
+    const url = `https://www.google.com/maps/search/?api=1&query=${lat},${lng}`
   return (
     <Link className='p-1 text-white my-3 inline-block bg-blue-600 w-full rounded-lg ' href={url} >عرض الموقع على الخريطة</Link>
   )
