4716759f8b3bc94391e2d7f4c5e55de8ab550ffc
othman <thman.saleh@gmail.com>
2024-09-04 18:41:40 +0400

    4r43rfr

--- app/swr/driverReports.js
+++ app/swr/driverReports.js
@@ -1,13 +1,15 @@
 
+'use client'
 import axios from 'axios';
-
 import useSWR from 'swr'
 
 export function swrDriverReports(){
-    const token=localStorage.getItem('token')
+    // const token=localStorage.getItem('token')
   
  const url ="https://express-rta.vercel.app/get-driver-reports-today"
   const fetcher = async () => {
+    const token=localStorage.getItem('token')
+
    const response = await axios.get(url,{
     headers: {
       authorization: `${token}`
