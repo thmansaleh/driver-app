be818007b435897a18b558188f1ccd6790ec1eba
othman <thman.saleh@gmail.com>
2024-09-06 16:38:18 +0400

    4tgr4f

--- app/components/tracking/Tracking.js
+++ app/components/tracking/Tracking.js
@@ -1,31 +1,37 @@
 import { getMycurrentLocation } from '@/app/services/helpers/getMycurrentLocation';
 import { updatCarLocation } from '@/app/services/updatCarLocation';
-import  { useEffect } from 'react'
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 function Tracking() {
-    const nida =useSelector(state=>state.account.account.nida)
-    useEffect(() => {
-        getMycurrentLocation( async(loc) => {
-            if (loc) {
-            //   setLocation(loc);
-            console.log(loc)
-            const data = await updatCarLocation(nida,loc.lat,loc.lng)
-            // await getData(loc)
-            console.log(data)
-            }
-          });
-      
-       
-     const getData=async(loc)=>{
-    //  console.log('from fu',loc)
-        // const data = await updatCarLocation(nida,loc.lat,loc.lng)
-        // setDistance(data.distance)
-        // setTime(data.time)
-     }
-    //  getData()
-    }, [])
+    const nida = useSelector(state => state.account.account.nida);
 
-    return null
+    useEffect(() => {
+        // Function to fetch and update location
+        const fetchAndUpdateLocation = async () => {
+            getMycurrentLocation(async (loc) => {
+                if (loc) {
+                    // console.log(loc);
+                    const data = await updatCarLocation(nida, loc.lat, loc.lng);
+                    // console.log(data)
+                }
+            });
+        };
+
+        // Call the function immediately once on component mount
+        fetchAndUpdateLocation();
+
+        // Set interval to call the function every 6 seconds (6000 milliseconds)
+        const intervalId = setInterval(() => {
+            fetchAndUpdateLocation();
+        }, 6000);
+
+        // Cleanup the interval on component unmount
+        return () => clearInterval(intervalId);
+
+    }, [nida]);
+
+    return null;
 }
-export default Tracking
+
+export default Tracking;
