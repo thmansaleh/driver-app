9cd0e17001529873e6938d5786d9275c4fdc7b86
Othman saleh <97992637+thmansaleh@users.noreply.github.com>
2024-09-16 15:39:31 +0400

    Update getMycurrentLocation.js

--- app/services/helpers/getMycurrentLocation.js
+++ app/services/helpers/getMycurrentLocation.js
@@ -1,13 +1,21 @@
-// locationUtils.js
 export function getMycurrentLocation(callback) {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
-      const location = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-      callback(location);
-    });
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const location = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        callback(location);
+      },
+      (error) => {
+        console.error("Error getting location", error);
+        callback(null);
+      },
+      {
+        enableHighAccuracy: true // This enables high accuracy mode
+      }
+    );
   } else {
     console.log("Geolocation is not supported by this browser.");
     callback(null);
